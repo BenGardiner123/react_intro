11eb5a8baafafa6ae0b84db1250337824bb4a05b
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2022-03-01 14:58:38 +1100

    use State and the updating process

--- react-intro/src/components/Expenses/ExpenseItem.js
+++ react-intro/src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseItem.css'
 import './ExpenseDate'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+
+    // here use state allows us access to renaming function through object destructuring
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+      setTitle("Updated");
+    }
+
     return (
         <Card className='expense-item'>
-           <ExpenseDate date={props.date}/>
+            <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
                 <h2>
-                    {props.title}
-                </h2> 
+                    {title}
+                </h2>
                 <div className='expense-item__price'>
                     $ {props.amount}
                 </div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
- 
+
 export default ExpenseItem;
