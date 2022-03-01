e178609d5ea39d4f170a3792cb2e0a7b7e782f33
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2022-03-01 13:54:56 +1100

    adding arrow function syntax isntead of calling the function keyword

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -1,36 +1,21 @@
-import logo from './logo.svg';
-import ExpenseItem from './components/ExpenseItem';
-import './App.css';
 
-function App() {
+import Expenses from "./components/Expenses/Expenses";
+
+
+const App = () => {
   const expenses = [
-    {title: "car insurance", amount: 385.84,  date: new Date(2021, 2, 28)},
-    {title: "new computer", amount: 1385.84,  date: new Date(2021, 3, 28)},
-    {title: "new superyacht", amount: 234444385.84,  date: new Date(2021, 5, 28)},
-    {title: "dog food", amount: 35.84,  date: new Date(2021, 6, 28)}
+    { title: "car insurance", amount: 385.84, date: new Date(2021, 2, 28) },
+    { title: "new computer", amount: 1385.84, date: new Date(2021, 3, 28) },
+    { title: "new superyacht", amount: 234444385.84, date: new Date(2021, 5, 28) },
+    { title: "dog food", amount: 35.84, date: new Date(2021, 6, 28) }
   ]
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-        <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
+
   );
 }
 
--- react-intro/src/components/Expenses/ExpenseItem.js
+++ react-intro/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,14 @@
+import React from 'react';
+
 import './ExpenseItem.css'
+import './ExpenseDate'
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 
 function ExpenseItem(props) {
-    // const expenseDate = new Date(2021, 2, 28);
-    // const expenseTitle = 'Car Insurance';
-    // const expenseAmount = 294.50;
-
-    
-
     return (
-        <div className='expense-item'>
-            <div>{props.date.toLoacleString()}</div>
-            <div></div>
-            <div></div>
-
+        <Card className='expense-item'>
+           <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>
                     {props.title}
@@ -21,8 +17,8 @@ function ExpenseItem(props) {
                     $ {props.amount}
                 </div>
             </div>
-        </div>
+        </Card>
     );
 }
-
+ 
 export default ExpenseItem;
