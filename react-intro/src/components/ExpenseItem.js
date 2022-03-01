cefd7b0adc8a1771bfc139fb88d44c22a363ddd7
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2022-03-01 11:56:43 +1100

    looking at JS in components

--- react-intro/src/components/ExpenseItem.js
+++ react-intro/src/components/ExpenseItem.js
@@ -9,13 +9,14 @@ function ExpenseItem(props) {
 
     return (
         <div className='expense-item'>
-            <div>
-                {props.date.toISOString()}
-            </div>
+            <div>{props.date.toLoacleString()}</div>
+            <div></div>
+            <div></div>
+
             <div className='expense-item__description'>
                 <h2>
                     {props.title}
-                </h2>
+                </h2> 
                 <div className='expense-item__price'>
                     $ {props.amount}
                 </div>
