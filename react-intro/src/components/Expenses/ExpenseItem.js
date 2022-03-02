c13dda99077ea25be33b17db2da734e23abbac89
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2022-03-02 15:39:35 +1100

    adding the chart

--- react-intro/src/components/Expenses/ExpenseItem.js
+++ react-intro/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './ExpenseItem.css'
 import './ExpenseDate'
@@ -6,30 +6,17 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-
-    // here use state allows us access to renaming function through object destructuring
-    const [title, setTitle] = useState(props.title);
-
-    const clickHandler = () => {
-        setTitle("Updated");
-    }
-
     return (
-        <li>
-            <Card className='expense-item'>
-                <ExpenseDate date={props.date} />
-                <div className='expense-item__description'>
-                    <h2>
-                        {title}
-                    </h2>
-                    <div className='expense-item__price'>
-                        $ {props.amount}
-                    </div>
-                </div>
-            </Card>
-        </li>
-
+      <li>
+        <Card className='expense-item'>
+          <ExpenseDate date={props.date} />
+          <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
+          </div>
+        </Card>
+      </li>
     );
-}
+  };
 
 export default ExpenseItem;
